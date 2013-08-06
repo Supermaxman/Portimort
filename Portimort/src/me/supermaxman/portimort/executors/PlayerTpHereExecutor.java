7d6b36371057498eded465f056e05e6f844da1a5
Aoloebio <jmbeffel@yahoo.com>
2013-08-06 15:48:16 -0700

    Update to disable "vanished" player checks
    this one will actually compile, unlike my previous branch (silly me)

--- Portimort/src/me/supermaxman/portimort/executors/PlayerTpHereExecutor.java
+++ Portimort/src/me/supermaxman/portimort/executors/PlayerTpHereExecutor.java
@@ -18,7 +18,7 @@ public class PlayerTpHereExecutor extends BaseExecutor {
     	}
     	String s = args[0];
     	Player too = player.getServer().getPlayer(s);
-    	if(too==null) {
+    	if(too==null || too.getMetadata("vanished").toString().toLowerCase()=="true") {
     		player.sendMessage(Portimort.pxe+"Player not found.");//error, player not found
     		return;
     	}
